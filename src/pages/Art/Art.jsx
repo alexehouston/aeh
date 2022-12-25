e6a23c930e4bef602c60e212af14ebb80d505442
alexehouston <alexehouston@gmail.com>
2022-12-25 12:46:06 -0600

    add flickity

--- src/pages/Art/Art.jsx
+++ src/pages/Art/Art.jsx
@@ -1,16 +1,31 @@
+import Flickity from 'react-flickity-component'
 import './Art.css';
 
+const flickityOptions = {
+    initialIndex: 2,
+    autoPlay: true,
+    contain: true
+}
+
 export default function Art() {
+
     return (
-        <div className="art">
-            <img src="/assets/art/hatterene.jpeg" alt="" />
-            <img src="/assets/art/danny-beard.jpeg" alt="" />
-            <img src="/assets/art/rita-repulsa.jpeg" alt="" />
-            <img src="/assets/art/white-ranger.jpeg" alt="" />
-            <img src="/assets/art/power-rangers.jpeg" alt="" />
-            <img src="/assets/art/maleficent.jpeg" alt="" />
-            <img src="/assets/art/rosalina.jpeg" alt="" />
-            <img src="/assets/art/sminty-drop.jpeg" alt="" />
-        </div>
+        <Flickity
+            className={'carousel'}
+            elementType={'div'}
+            options={flickityOptions}
+            disableImagesLoaded={false}
+            reloadOnUpdate
+            static
+        >
+                <img src="/assets/art/hatterene.jpeg" alt="" />
+                <img src="/assets/art/danny-beard.jpeg" alt="" />
+                <img src="/assets/art/rita-repulsa.jpeg" alt="" />
+                <img src="/assets/art/white-ranger.jpeg" alt="" />
+                <img src="/assets/art/power-rangers.jpeg" alt="" />
+                <img src="/assets/art/maleficent.jpeg" alt="" />
+                <img src="/assets/art/rosalina.jpeg" alt="" />
+                <img src="/assets/art/sminty-drop.jpeg" alt="" />
+        </Flickity>
     );
 }
