ac115395c4420a7b0e6b36fcb25bac64f13f029b
alexehouston <alexehouston@gmail.com>
2022-12-29 00:16:58 -0600

    update animations

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,31 +1,19 @@
 import './Nav.css';
 
 export default function Nav({ currentPage, setCurrentPage }) {
-    
-    function renderHome() {
-        setCurrentPage('home');
-    }
-
-    function renderArt() {
-        setCurrentPage('art');
-    }
-
-    function renderContact() {
-        setCurrentPage('contact');
-    }
-
     return (
         <>
             <ul className="nav">
-                <li onClick={renderHome}>home</li>
-                <li className="dropdown">work
+                <li onClick={() => setCurrentPage('home')}><span className="material-symbols-outlined size-40">home</span></li>
+                <li className="dropdown">
+                    <span className="material-symbols-outlined size-40">work</span>
                     <ul className="dropdown-content">
                     <li><a href="https://kpxaeh.com" target="_blank" rel="noreferrer">Hair Bookings</a></li>
-                        <li onClick={renderArt}>Artwork</li>
+                        <li onClick={() => setCurrentPage('art')}>Artwork</li>
                         <li><a href="https://alexehouston.github.io/portfolio/" target="_blank" rel="noreferrer">Dev Portfolio</a></li>
                     </ul>
                 </li>
-                <li onClick={renderContact}>contact</li>
+                <li onClick={() => setCurrentPage('contact')}><span className="material-symbols-outlined size-40">mail</span></li>
             </ul>
         </>
     );
--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -2,22 +2,22 @@ import './Title.css';
 
 export default function Title() {
     return (
-        <div className="title">
-            <div className="glitch-container">
-                <h1 className="glitch"><span className="alex"></span>Alex</h1>
-                <h1 className="glitch"><span className="alex"></span>Alex</h1>
-                <h1 className="glitch"><span className="alex"></span>Alex</h1>
+        <div className="Title">
+            <div class="glitch-container">
+                <h1 class="glitch"><span class="alex">Alex</span></h1>
+                <h1 class="glitch"><span class="alex">Alex</span></h1>
+                <h1 class="glitch"><span class="alex">Alex</span></h1>
             </div>
-            <div className="glitch-container">
-                <h1 className="glitch"><span className="houston">Houston</span></h1>
-                <h1 className="glitch"><span className="houston">Houston</span></h1>
-                <h1 className="glitch"><span className="houston">Houston</span></h1>
+            <div class="glitch-container">
+                <h1 class="glitch"><span class="houston">Houston</span></h1>
+                <h1 class="glitch"><span class="houston">Houston</span></h1>
+                <h1 class="glitch"><span class="houston">Houston</span></h1>
             </div>
-            <div className="glitch-container">
-                <h1 className="glitch"><span className="developer">Developer | Stylist | Artist</span></h1>
-                <h1 className="glitch"><span className="developer">Developer | Stylist | Artist</span></h1>
-                <h1 className="glitch"><span className="developer">Developer | Stylist | Artist</span></h1>
+            <div class="glitch-container">
+                <h1 class="glitch"><span class="developer">SOFTWARE&nbsp;DEVELOPER</span></h1>
+                <h1 class="glitch"><span class="developer">SOFTWARE&nbsp;DEVELOPER</span></h1>
+                <h1 class="glitch"><span class="developer">SOFTWARE&nbsp;DEVELOPER</span></h1>
             </div>
         </div>
-    )
+    );
 }
