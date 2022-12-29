7795af2675855c5f16be1bdd2db8a450aa3abcbc
alexehouston <alexehouston@gmail.com>
2022-12-29 00:04:08 -0600

    update animations

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,17 +3,18 @@ import Title from '../../components/Title/Title';
 import Nav from '../../components/Nav/Nav';
 import Contact from '../../pages/Contact/Contact';
 import Art from '../../pages/Art/Art';
+import 'animate.css';
 import './App.css';
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState('home');
 
   return (
-    <>
+    <div className="App">
       <Nav currentPage={currentPage} setCurrentPage={setCurrentPage} />
       {currentPage === 'contact' ? ( <Contact /> )
       : currentPage === 'art' ? ( <Art />)
       : ( <><Title /></> )}
-    </>
+    </div>
   );
 }
--- src/pages/Art/Art.jsx
+++ src/pages/Art/Art.jsx
@@ -3,32 +3,28 @@ import { Navigation, Autoplay } from 'swiper';
 import 'swiper/css/bundle';
 import './Art.css';
 
-const flickityOptions = {
-    initialIndex: 2,
-    autoPlay: true,
-    contain: true
-}
-
 export default function Art() {
 
     return (
-        <Swiper
-            modules={[Navigation, Autoplay]}
-            navigation={true}
-            slidesPerView={3}
-            autoplay={{
-                delay: 2000,
-                disableOnInteraction: false,
-            }}
-        >
-            <SwiperSlide><img src="/assets/art/hatterene.jpeg" alt="" /></SwiperSlide>
-            <SwiperSlide><img src="/assets/art/danny-beard.jpeg" alt="" /></SwiperSlide>
-            <SwiperSlide><img src="/assets/art/rita-repulsa.jpeg" alt="" /></SwiperSlide>
-            <SwiperSlide><img src="/assets/art/white-ranger.jpeg" alt="" /></SwiperSlide>
-            <SwiperSlide><img src="/assets/art/power-rangers.jpeg" alt="" /></SwiperSlide>
-            <SwiperSlide><img src="/assets/art/maleficent.jpeg" alt="" /></SwiperSlide>
-            <SwiperSlide><img src="/assets/art/rosalina.jpeg" alt="" /></SwiperSlide>
-            <SwiperSlide><img src="/assets/art/sminty-drop.jpeg" alt="" /></SwiperSlide>
-        </Swiper>
+        <div className="Art">
+            <Swiper
+                modules={[Navigation, Autoplay]}
+                navigation={true}
+                slidesPerView={3}
+                autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false,
+                }}
+            >
+                <SwiperSlide><img src="/assets/art/hatterene.jpeg" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="/assets/art/danny-beard.jpeg" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="/assets/art/rita-repulsa.jpeg" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="/assets/art/white-ranger.jpeg" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="/assets/art/power-rangers.jpeg" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="/assets/art/maleficent.jpeg" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="/assets/art/rosalina.jpeg" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="/assets/art/sminty-drop.jpeg" alt="" /></SwiperSlide>
+            </Swiper>
+        </div>
     );
 }
